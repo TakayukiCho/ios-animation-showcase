18a3cd5da029be4cf6517c8814d226e38e4c0fe8
TakayukiCho <taka.3365@gmail.com>
2018-07-25 21:06:25 +0900

    Initial Commit

---
+++ ios-animation-showcase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-animation-showcase
+//
+//  Created by tc on 2018/07/25.
+//  Copyright © 2018 tc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
